f7445cf55de0570d96548171676a2d464cad7d2b
Jon Juliusson <Jon Juliusson>
2016-03-09 17:55:28 +0000

    Grunnur

--- src/components/sellers/SellersController.js
+++ src/components/sellers/SellersController.js
@@ -4,5 +4,46 @@ angular.module("project3App").controller("SellersController",
 function SellersController($scope, AppResource) {
 	// TODO: load data from AppResource! Also, add other methods, such as to
 	// add/update sellers etc.
+//$scope.sellers = {};
+	$scope.seller = [
+		{
+			name: "nonni"
+		},
+		{
+			name: "oli"
+		}
+	];
+
+//	$scope.sellers.Name = "nonni";
+
+
+
+	$scope.isLoading = true;
+
+AppResource.getSellers().success(function(sellers){
+	$scope.sellers = sellers;
+	$scope.isLoading = false;
+}).error(function(){
+	$scope.isLoading = false;
+});
+
+$scope.onAddSeller = function onAddSeller(){
+/*
+SellerDlg.show().then(function(seller){
+	AppResource.addSeller(peterSeller).success(function(seller){
+			var newSeller = seller;
+	}).error(function(){
+	//		centrisNotify.error("sellers.Messages.SaveFailed");
+		// TODO: hvað gerum við?
+	});
+});*/
+
+var peterSeller = {
+	name: "Peter Sellers",
+	category: "Movies",
+	imagePath: "http://www.meredy.com/sellerspeter03.jpg"
+};
+
+};
 
 });
